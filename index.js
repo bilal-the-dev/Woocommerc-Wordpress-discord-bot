4a2f86f9d35b6a64db42f4eb6d263ccbf3571278
Bilal <bilalfarooq6001@gmail.com>
2024-03-24 14:30:45 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const { Client, IntentsBitField, Partials } = require("discord.js");
+const WOK = require("wokcommands");
+const { DefaultCommands } = WOK;
+const path = require("path");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./.env" });
+
+const client = new Client({
+	intents: [
+		IntentsBitField.Flags.Guilds,
+		IntentsBitField.Flags.GuildMessages,
+		IntentsBitField.Flags.GuildMembers,
+		IntentsBitField.Flags.MessageContent,
+	],
+	partials: [Partials.Channel],
+});
+
+client.on("ready", async () => {
+	console.log(`${client.user.username} is running 🧶`);
+
+	new WOK({
+		client,
+		commandsDir: path.join(__dirname, "./commands"),
+		events: {
+			dir: path.join(__dirname, "events"),
+		},
+		disabledDefaultCommands: [
+			DefaultCommands.ChannelCommand,
+			DefaultCommands.CustomCommand,
+			DefaultCommands.Prefix,
+			DefaultCommands.RequiredPermissions,
+			DefaultCommands.RequiredRoles,
+			DefaultCommands.ToggleCommand,
+		],
+		cooldownConfig: {
+			errorMessage: "Please wait {TIME} before doing that again.",
+			botOwnersBypass: false,
+			dbRequired: 300,
+		},
+	});
+});
+client.login(process.env.TOKEN);
